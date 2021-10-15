0704eeb747348153b1391080fcbaa47bff523fe4
Suwilanj Chipofya <90550776+suwilanji-chipofya@users.noreply.github.com>
2021-10-15 22:23:40 +0200

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,10 @@
 
 import Layout from '../components/layout'
-import styles from '../styles/util.module.css'
+import utilStyles from '../styles/util.module.css'
 export default function Custom404() {
     return (
         <Layout>
-        <img src="/images/404.jpeg" style={{styles.pagenotfound}} alt="Even the 404 Image was not found" />
+        <img src="/images/404.jpeg" style={{utilStyles.pagenotfound}} alt="Even the 404 Image was not found" />
         </Layout>
         )
 }
